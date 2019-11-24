9a69a644b9ee04affcee0329fd763cef2e184530
vladhmelevskiy1234765 <vladhmelevskiy1234765@gmail.com>
2019-11-24 16:16:08 +0200

    first up

---
+++ ClientSynchronizationFiles/ClientSynchronizationFiles/Connection.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSynchronizationFiles
+{
+    class Connection
+    {
+        public Connection()
+        {
+
+        }
+
+        private const string ip2 = "192.168.1.240";
+        private const int port = 2048;
+        private Socket tcpSocket;
+        private string pathToFolder = "D:\\temp\\ClientDirectory";
+
+        private byte[] buffer;
+        private StringBuilder data;
+        List<FileStruct> filesPathsAndTimeCreateOrChangeFiles = new List<FileStruct>();
+
+        public void Client()
+        {
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip2), port);
+            using (tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    tcpSocket.Connect(tcpEndPoint);
+                    AddFilesAndThemTime();
+                    NewFiles newFiles = new NewFiles(filesPathsAndTimeCreateOrChangeFiles, tcpSocket);
+                    newFiles.FirstCheckFiles();
+                    while (true)
+                    {
+                        ChangeFiles();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+        private void ChangeFiles()
+        {
+            AnswerServer();
+            if (data.ToString() == "delete")
+            {
+                SendMessage("?");
+                AnswerServer();
+                var filesPaths = Split();
+                SendMessage("?");
+                DeleterFiles(filesPaths);
+            }
+            else if (data.ToString() == "new")
+            {
+                SendMessage("?");
+                AnswerServer();
+                var filesPaths = Split();
+                SendMessage("?");
+                AddNewOrChangeFiles(filesPaths, false);
+            }
+            else if (data.ToString() == "change")
+            {
+                SendMessage("?");
+                AnswerServer();
+                var filesPaths = Split();
+                SendMessage("?"); // add DeleterFiles(filesPaths);
+                AddNewOrChangeFiles(filesPaths, true);
+            }
+        }
+        private void AddNewOrChangeFiles(string[] filesPaths, bool change)
+        {
+            if (change)
+            {
+                DeleterFiles(filesPaths); // delete DeleterFiles(filesPaths);
+            }
+            for (int i = 0; i < filesPaths.Length - 1; i++)
+            {
+                AnswerServer();
+                if (data.ToString() == "?")
+                {
+                    File.WriteAllText(filesPaths[i], "");
+                }
+                else
+                {
+                    File.WriteAllText(filesPaths[i], data.ToString());
+                }
+                SendMessage("?");
+            }
+        }
+        private void DeleterFiles(string[] filesPaths)
+        {
+            for (int i = 0; i < filesPaths.Length - 1; i++)
+            {
+                File.Delete(filesPaths[i]);
+            }
+        }
+        private string[] Split()
+        {
+            return data.ToString().Split('?');
+        }
+        private void AddFilesAndThemTime()
+        {
+            var filesPaths = new List<string>();
+            filesPaths.AddRange(Directory.GetFiles(pathToFolder));
+            for (int i = 0; i < filesPaths.Count; i++)
+            {
+                FileStruct file = new FileStruct();
+                file.filePath = filesPaths[i];
+                file.timeCreateOrChangeFile = File.GetLastWriteTime(filesPaths[i]);
+                filesPathsAndTimeCreateOrChangeFiles.Add(file);
+            }
+        }
+        private void AnswerServer()
+        {
+            buffer = new byte[256];
+            data = new StringBuilder();
+            do
+            {
+                var size = tcpSocket.Receive(buffer);
+                data.Append(Encoding.ASCII.GetString(buffer, 0, size));
+            } while (tcpSocket.Available > 0);
+        }
+        private void SendMessage(string message)
+        {
+            tcpSocket.Send(Encoding.ASCII.GetBytes(message));
+        }
+    }
+}
---
+++ ClientSynchronizationFiles/ClientSynchronizationFiles/NewFiles.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSynchronizationFiles
+{
+    class NewFiles
+    {
+        public NewFiles(List<FileStruct> filesPathsAndTimeCreateOrChangeFiles, Socket tcpSocket)
+        {
+            this.filesPathsAndTimeCreateOrChangeFiles = filesPathsAndTimeCreateOrChangeFiles;
+            this.tcpSocket = tcpSocket;
+        }
+
+        private Socket tcpSocket;
+        private string pathToFolder = "D:\\temp\\ClientDirectory";
+
+        private byte[] buffer;
+        private StringBuilder data;
+        List<FileStruct> filesPathsAndTimeCreateOrChangeFiles = new List<FileStruct>(); // delete  = new List<FileStruct>()
+
+        public void FirstCheckFiles()
+        {
+            AnswerServer();
+            ReciveNewFiles();
+        }
+        private void ReciveNewFiles()
+        {
+            var newFilesCount = FindNewFiles();
+            for (int i = 0; i < newFilesCount.Count; i++)
+            {
+                AnswerServer();
+                File.Delete(newFilesCount[i]);
+                if (data.ToString() != "?")
+                {
+                    File.AppendAllText(newFilesCount[i], data.ToString());
+                }
+                else
+                {
+                    File.AppendAllText(newFilesCount[i], "");
+                }
+                SendMessage("?");
+            }
+        }
+        private List<string> FindNewFiles()
+        {
+            var (filesPathsOnServer, listTimeCreateOrChangeFilesOnServer) = Split();
+            var newFiles = new List<string>();
+            for (int i = 0; i < filesPathsOnServer.Length; i++)
+            {
+                var haveFile = false;
+                for (int j = 0; j < filesPathsAndTimeCreateOrChangeFiles.Count; j++)
+                {
+                    if (filesPathsOnServer[i] == filesPathsAndTimeCreateOrChangeFiles[j].filePath)
+                    {
+                        if (listTimeCreateOrChangeFilesOnServer[i] <= filesPathsAndTimeCreateOrChangeFiles[j].timeCreateOrChangeFile)
+                        {
+                            haveFile = true;
+                        }
+                        continue;
+                    }
+                }
+                if (!haveFile)
+                {
+                    AddFilesAndThemTimeToList(filesPathsOnServer[i]);
+                    newFiles.Add(filesPathsOnServer[i]);
+                }
+            }
+            var newFileInStringBuilder = new StringBuilder();
+            foreach (var newFile in newFiles)
+            {
+                newFileInStringBuilder.Append($"{newFile}?");
+            }
+            if (newFileInStringBuilder.Length != 0)
+            {
+                SendMessage(newFileInStringBuilder.ToString());
+            }
+            else
+            {
+                SendMessage("?");
+            }
+            return newFiles;
+        }
+        private void AddFilesAndThemTimeToList(string filePath)
+        {
+            FileStruct file = new FileStruct();
+            file.filePath = filePath;
+            file.timeCreateOrChangeFile = File.GetLastWriteTime(filePath);
+            filesPathsAndTimeCreateOrChangeFiles.Add(file);
+        }
+        private (string[] filesPathsOnServer, DateTime[] listTimeCreateOrChangeFilesOnServer) Split()
+        {
+            var filesPathsOnServerStringArray = data.ToString().Split('?');
+            var listTimeCreateOrChangeFilesOnServerArray = filesPathsOnServerStringArray[filesPathsOnServerStringArray.Length - 1].Split('*');
+            Array.Resize(ref filesPathsOnServerStringArray, filesPathsOnServerStringArray.Length - 1);
+            var filesPathsOnServerArray = new DateTime[listTimeCreateOrChangeFilesOnServerArray.Length - 1];
+            for (int i = 0; i < filesPathsOnServerArray.Length; i++)
+            {
+                filesPathsOnServerArray[i] = Convert.ToDateTime(listTimeCreateOrChangeFilesOnServerArray[i]);
+            }
+            return (filesPathsOnServerStringArray, filesPathsOnServerArray);
+        }
+        private void AnswerServer()
+        {
+            buffer = new byte[256];
+            data = new StringBuilder();
+            do
+            {
+                var size = tcpSocket.Receive(buffer);
+                data.Append(Encoding.ASCII.GetString(buffer, 0, size));
+            } while (tcpSocket.Available > 0);
+        }
+        private void SendMessage(string message)
+        {
+            tcpSocket.Send(Encoding.ASCII.GetBytes(message));
+        }
+    }
+}
